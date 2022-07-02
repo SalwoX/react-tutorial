b158ea6271a4d4e2a3e8fb23eac15b013684a4b6
Ömer <omertufekci97@gmail.com>
2022-07-02 16:39:38 +0300

    first commit

---
+++ react-tutorial-api/src/main/java/com/eyox/reacttutorialapi/ReactTutorialApiApplication.java
@@ -0,0 +1,13 @@
+package com.eyox.reacttutorialapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactTutorialApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactTutorialApiApplication.class, args);
+	}
+
+}
---
+++ react-tutorial-api/src/test/java/com/eyox/reacttutorialapi/ReactTutorialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eyox.reacttutorialapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactTutorialApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
